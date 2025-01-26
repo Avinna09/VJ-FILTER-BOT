adec8f9dde98d7cc38441391a70535a6278c998c
Avinna09 <cmahalik216@gmail.com>
2025-01-26 23:16:18 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -246,7 +246,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<a href='https://t.me/movies_hd_01'>{file_name}</a><b>📂Fɪʟᴇɴᴀᴍᴇ : {file_name}
 
 <b> Size ⚙️: {file_size}</b>""" 
 
