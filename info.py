98b1f6f5e2853797522ef5363039ce0041729618
Avinna09 <cmahalik216@gmail.com>
2025-05-05 23:29:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'short2url.in')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'b8785d31f2262c0c7673d012d5cbadb8d5d9e35c')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
